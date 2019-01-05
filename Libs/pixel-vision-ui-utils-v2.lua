cc13af9c0a44a598e763f74d86e9fb67cfbd3461
Jesse Freeman <jessefreeman@gmail.com>
2019-01-05 13:49:35 -0500

    Adding explode settings API to utils.

--- Libs/pixel-vision-ui-utils-v2.lua
+++ Libs/pixel-vision-ui-utils-v2.lua
@@ -107,4 +107,15 @@ function dump(o)
   end
 end
 
+function ExplodeSettings(str)
+  local div = ","
+
+  local pos, arr = 0, {}
+  for st, sp in function() return string.find(str, div, pos, true) end do
+    table.insert(arr, tonumber(string.sub(str, pos, st - 1)))
+    pos = sp + 1
+  end
+  table.insert(arr, tonumber(string.sub(str, pos)))
+  return arr
+end
 -- Editor Helpers
