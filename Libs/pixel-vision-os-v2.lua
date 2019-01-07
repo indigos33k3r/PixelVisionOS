6780e82edb1c2eb95bd137c7a129f2c7a2b405a1
Jesse Freeman <jessefreeman@gmail.com>
2019-01-06 21:00:23 -0500

    Updating version.

--- Libs/pixel-vision-os-v2.lua
+++ Libs/pixel-vision-os-v2.lua
@@ -42,7 +42,7 @@ function PixelVisionOS:Init()
   -- Create message bar instance
   _pixelVisionOS.messageBar = _pixelVisionOS:CreateMessageBar(7, 230, 60)
 
-  _pixelVisionOS.version = "v2.1"
+  _pixelVisionOS.version = "v2.2"
 
   return _pixelVisionOS
 
